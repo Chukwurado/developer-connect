44aebc30392fba26a15db24420b436be5a5f91c4
Chukwurado98 <chukwurado.umeaka98@gmail.com>
2019-09-23 19:38:23 -0400

    Add route files

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const db = config.get("mongoURI"); //uses the config module to get mongoURI
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("MongoDB Connected"); //when connected successfully
+    } catch (e) {
+        console.error(e.message);
+        process.exit(1); //Exit process with faliure
+    }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 
+const connectDB = require("./config/db");
+
 const app = express();
 
+//Connect Database
+connectDB();
+
 app.get("/", (req, res) => res.send("API running"));
+
+const usersRoute = require("./routes/api/users");
+const profileRoute = require("./routes/api/profile");
+const postsRoute = require("./routes/api/posts");
+const authRoute = require("./routes/api/auth");
+
+//Define routes
+app.use("/api/users", usersRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/auth", authRoute);
 
 const PORT = process.env.PORT || 5000;
 
